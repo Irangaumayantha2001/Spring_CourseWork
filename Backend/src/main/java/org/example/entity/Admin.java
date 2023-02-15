c9a6484066578ab915d7b1d6a4ae1544f209ccd3
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-02-15 21:42:15 +0530

    add driver html file and css file

--- Backend/src/main/java/org/example/entity/Admin.java
+++ Backend/src/main/java/org/example/entity/Admin.java
@@ -8,15 +8,19 @@ import lombok.ToString;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @ToString
 @Entity
-public class Login {
+public class Admin {
     @Id
-    private String loginId;
+    private String adminId;
+    private String name;
+    private String address;
+    private String contact;
+    private String email;
     private String username;
     private String password;
-    private String role;
 }
