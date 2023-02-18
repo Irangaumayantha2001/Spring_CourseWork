df42f338a8f978e68bdff07b67128f187b831396
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-02-18 21:20:48 +0530

    add customer dto

--- Backend/src/main/java/org/example/controller/CustomerController.java
+++ Backend/src/main/java/org/example/controller/CustomerController.java
@@ -1,4 +1,12 @@
 package org.example.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/customer")
+@CrossOrigin
 public class CustomerController {
+
 }
--- Backend/src/main/java/org/example/service/impl/CustomerServiceImpl.java
+++ Backend/src/main/java/org/example/service/impl/CustomerServiceImpl.java
@@ -1,4 +1,5 @@
 package org.example.service.impl;
 
 public class CustomerServiceImpl {
+
 }
