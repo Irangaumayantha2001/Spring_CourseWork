d5716ad8d38f40cb5a61fafaf863dc1a023cb728
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-02-19 19:59:04 +0530

    driver contend update

--- Backend/src/main/java/org/example/service/DriverService.java
+++ Backend/src/main/java/org/example/service/DriverService.java
@@ -1,4 +1,33 @@
 package org.example.service;
 
+import java.util.List;
+
 public interface DriverService {
+    void saveDriver(DriverDTO dto);
+
+    void updateDriver(DriverDTO dto);
+
+    void deleteDriver(String licenceNo);
+
+    DriverDTO searchDriver(String licenceNo);
+
+    List<DriverDTO> getAllDrivers();
+
+    boolean findDriverByUsername(String username);
+
+    boolean findDriverByPassword(String password);
+
+    DriverDTO findDriverByUsernameAndPassword(String username, String password);
+
+    void updateDriverNonAvailable(String licenceNo);
+
+    void updateDriverAvailable(String licenceNo);
+
+    List<DriverDTO> getAllAvailableDrivers();
+
+    List<DriverDTO> getRandomDriver();
+
+    List<DriverDTO> getAllNonAvailableDrivers();
+
+    int getCountOfDriversByStatus(boolean availability);
 }
